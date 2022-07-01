be9bfd27807a16269629338f8442c762b84daf48
Mahdi MirMohamadi <mahdimirmohamadi13@gmail.com>
2022-07-01 16:13:49 +0430

    contact component handled

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Intro from "./components/intro/Intro.jsx";
 import About from "./components/about/About.jsx";
 import ProductList from "./components/productList/ProductList.jsx";
+import Contact from "./components/contact/Contact.jsx";
 const App = () => {
   return (
     <div>
       <Intro />
       <About />
       <ProductList />
+      <Contact />
     </div>
   );
 };
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,48 @@
+import React, { useRef } from "react";
+import "./Contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
+
+const Contact = () => {
+  const formRef = useRef();
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's discuss your project</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="phone image" className="c-icon" />
+              +98 910 0770 673
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Email} alt="email icon image" />
+              mahdimirmohamadi13@gmail.com
+            </div>
+            <div className="c-info-item">
+              <img className="c-icon" src={Address} alt="address icon image" />
+              Tehran, Iran <br></br>Sohrevardi Shomali
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <p className="c-desc">
+            <b>Want to Hire me?</b> Get in touch. Always available for Good
+            Hiring Opputunities
+          </p>
+          <form ref={formRef}>
+            <input type="text" placeholder="Name" name="user_name" />
+            <input type="text" placeholder="Subject" name="user_subject" />
+            <input type="text" placeholder="Email" name="user_email" />
+            <textarea rows="5" placeholder="message..." />
+            <button>Submit </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
